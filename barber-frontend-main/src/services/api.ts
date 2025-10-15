7cf972b5e5b7f855f832414ede7f87a01b902993
HallefHLVieira <hallef.vieira@hotmail.com>
2025-10-15 00:01:26 -0300

    update migration to remove default values

--- barber-frontend-main/src/services/api.ts
+++ barber-frontend-main/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://31.220.97.248:3333",
 })
 
 export const apiKey = "95e51a37c6e94e1e95e165534241601"
